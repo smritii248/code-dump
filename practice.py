36974598067028930e23d6530a50649a1b616b97
Smriti Joshi <joshismriti2003@gmail.com>
2025-06-14 08:51:20 +0545

    Your commit message

---
+++ practice.py
@@ -0,0 +1,31 @@
+
+import matplotlib.pyplot as plt 
+x=[1, 2 ,3 ,4 ,5 ]
+y=[10, 20, 30, 40, 50]
+ 
+plt.plot(x,y)
+plt.title("Sample Plot")
+plt.xlabel("X Axis ")
+plt.ylabel("Y Axis ")
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
