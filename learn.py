0dc23917277d99e386fb20468e1a0eedb6dbf2a5
Smriti Joshi <joshismriti2003@gmail.com>
2025-06-20 15:10:26 +0545

    Initial commit

---
+++ learn.py
@@ -0,0 +1,15 @@
+
+# Build histogram with 5 bins
+plt.hist(life_exp, bins = 5)
+
+# Show and clear plot
+plt.show()
+plt.clf()
+
+# Build histogram with 20 bins
+plt.hist(life_exp, bins = 20)
+
+# Show and clear plot again
+plt.show()
+
+
--- main.c
+++ main.c
@@ -1,28 +1,23 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
 void dectobinary(int nbit, int num, int arry[]) {
 for (int i = nbit - 1; i >= 0; i--) {
 arry[i] = num % 2;
 num /= 2;
 }
 }
-
 void twoComplement(int arry[], int nbit) {
 int carry = 1;
 for (int i = 0; i < nbit; i++) {
 arry[i] = !arry[i];
 }
-
 for (int i = nbit - 1; i >= 0; i--) {
 int bitsum = arry[i] + carry;
 arry[i] = bitsum % 2;
 carry = bitsum / 2;
 }
 }
-
 void showbinary(int *p, int size) {
 for (int i = 0; i < size; i++) {
 printf("%d", *(p + i));
@@ -39,7 +34,6 @@ carry = bitsum / 2;
 }
 return carry;
 }
-
 void binarytodec(int arry[], int nbit, int isSubtraction) {
 int result = 0;
 if (isSubtraction && arry[0] == 1) {
@@ -60,45 +54,36 @@ result += arry[i] * pow(2, (nbit - 1 - i));
 
 printf("The decimal equivalent is %d", result);
 }
-
 int main() {
 int num1, num2, arry1[10] = {0}, arry2[10] = {0}, sum[10] = {0}, nbit, status;
 printf("Enter number of bits: ");
 scanf("%d", &nbit);
 printf("Enter the two number: ");
 scanf("%d %d", &num1, &num2);
-
 int isSubtraction = (num1 < 0 || num2 < 0);
-
 if (num1 < 0) {
 dectobinary(nbit, abs(num1), arry1);
 twoComplement(arry1, nbit);
 } else {
 dectobinary(nbit, num1, arry1);
 }
-
 if (num2 < 0) {
 dectobinary(nbit, abs(num2), arry2);
 twoComplement(arry2, nbit);
 } else {
 dectobinary(nbit, num2, arry2);
 }
-
 printf("Binary of %d: ",num1);
 showbinary(arry1, nbit);
 printf("Binary of %d: ",num2);
 showbinary(arry2, nbit);
-
 status = addbinary(arry1, arry2, nbit, sum);
 printf("The sum is: ");
 showbinary(sum, nbit);
-
 if (status == 1 & !isSubtraction) {
 printf("with carry overflow of 1\n");
 }
-
 binarytodec(sum, nbit, isSubtraction);
-
 return 0;
 }
 
